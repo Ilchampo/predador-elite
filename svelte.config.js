9ac0e962deb8e2dac783f3360657d7a7e9e13246
Pablo Beltran <pablo.beltran@Pablos-MacBook-Pro.local>
2025-03-13 18:59:05 -0500

    Added vercel adapter and changed favicon to png

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,20 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
+
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			images: {
+				sizes: [640, 828, 1200, 1920, 3840],
+				formats: ['image/avif', 'image/webp', 'image/jpeg', 'image/png'],
+				minimumCacheTTL: 300,
+				domains: ['example-app.vercel.app']
+			}
+		})
 	}
 };
 
